bcc02762d15918a5419e62740eb37aa52e581908
Aetheronn <125783573+Aetheronn@users.noreply.github.com>
2023-03-05 21:08:09 +0530

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,19 @@
 local api = {}
 
-function api.print(txt)
-    print(txt)
+local api.Spoofs = {}
+
+function api.Spoofs.SpoofValue(valueName, returnValue)
+    local mt = getrawmetatable(game)
+    setreadonly(mt, false)
+    local old_index = mt.__index   
+    mt.__index = function(a,b)
+        if tostring(a) == valueName then
+            if tostring(b) == "Value" then
+              return returnValue
+            end
+        end
+        return old_index(a,b)
+    end
 end
 
 return api
